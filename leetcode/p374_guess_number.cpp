89e7e3a0de74b499bfa369411f5443d43aeccbcf
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2022-04-29 11:49:31 -0500

    Create p374_guess_number.cpp

---
+++ leetcode/p374_guess_number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long guessNumber(int n) {
+        long left_pointer = 0;
+        long right_pointer = n;
+        
+        while (left_pointer <= right_pointer) {
+            long mid = (left_pointer + right_pointer) / 2;
+            if (guess(mid) == 0)
+                return mid;
+            else if (guess(mid) == -1)//higher
+                right_pointer = mid - 1;
+            else if (guess(mid) == 1)//lower
+                left_pointer = mid + 1;
+        }
+        return -1;
+    }
+};
