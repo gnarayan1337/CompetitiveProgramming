49edb4f0adc3020e647bf5fe62cef38ffd5bf152
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2022-05-06 11:09:16 -0500

    Create p1523_count_odd_ nums.cpp

---
+++ leetcode/p1523_count_odd_ nums.cpp
@@ -0,0 +1,13 @@
+//author: gautam narayan
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int count = 0;
+        for (int i = low; i < high + 1; i++) {
+            if (i % 2 != 0)
+                count++;
+        }
+        return count;
+    }
+};
