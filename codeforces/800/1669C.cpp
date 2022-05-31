1c0cd3e457df903ddd44ddd99f1eb235cad5ee2a
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2022-05-31 17:50:36 -0500

    Create 1669C.cpp

---
+++ codeforces/800/1669C.cpp
@@ -0,0 +1,91 @@
+// author : gautam narayan
+// REDEMPTION - done in less than 27 minutes
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t{}; cin >> t;
+    while (t--) {
+        int n{}; cin >> n;
+        vector<int> arr{};
+        for (int i = 0; i < n; i++) {
+            int x{}; cin >> x;
+            arr.push_back(x);
+        }
+        /*
+        bool isAll = true;
+        for (auto x : arr){
+            if (x % 2 != 0) {
+                isAll = false;
+                break;
+            }
+        }
+        if (isAll == true) {
+            cout << "YES" << endl;
+            continue;
+        }
+        else {
+            cout << "NO" << endl;
+            continue;
+            }
+        */
+        
+        bool isAllEven = true;
+        bool isAllOdd = true;
+
+        bool isOddEven = false;
+        if (arr[1] % 2 == 0)
+            isOddEven = true;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0)
+                continue;
+            if (isOddEven == true) {
+                if (arr[i] % 2 != 0) {
+                    isAllOdd = false;
+                    break;
+                }
+                else
+                    continue;
+            }
+            else {
+                if (arr[i] % 2 == 0) {
+                    isAllOdd = false;
+                    break; }
+            }
+        }
+
+        bool isEvenEven = false;
+        if (arr[0] % 2 == 0)
+            isEvenEven = true; 
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 != 0)
+                continue;
+            if (isEvenEven == true) {
+                if (arr[i] % 2 != 0) {
+                    isAllEven = false;
+                    break;
+                }
+                else 
+                    continue;
+            }
+            else {
+                if (arr[i] % 2 == 0) {
+                    isAllEven = false;
+                    break;
+                }
+            }
+        }
+
+        if (isAllEven == true and isAllOdd == true) {
+            cout << "YES" << endl;
+        }
+        else
+            cout << "NO" << endl;
+    }
+    
+    return 0;
+}
