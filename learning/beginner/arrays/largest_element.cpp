1527d517117c40ef563a76d99060128091cf1b10
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2022-07-03 15:56:06 -0500

    Create largest_element.cpp

---
+++ learning/beginner/arrays/largest_element.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSumSubarray(vector<int> A) {
+    
+    int current_sum = 0;
+    int largest_sum = 0;
+
+    //to understand just do this in your head runs in o(n) time best out of the 3 other methods
+    //bruteforce runs in n^3, prefix runs in n^2, this runs in n.
+
+    for (int i = 0; i < A.size(); i++) {
+        current_sum = current_sum + A[i];
+        if (current_sum < 0 ) {
+            current_sum = 0;
+        }
+
+        largest_sum = max(largest_sum, current_sum);
+    }
+    return largest_sum;
+
+
+}
+
+int main() {
+
+    vector<int> A {1, -2, 3, 4, 4, -2};
+    vector<int> B {5, 0, -1, 0, 1, 2, -1};
+    cout << maxSumSubarray(B);
+
+}
