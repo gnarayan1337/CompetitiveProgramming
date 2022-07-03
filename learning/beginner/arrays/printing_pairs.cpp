91ce677ea3f42478f982aa673e276f9f2b9cf31a
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2022-07-03 15:56:21 -0500

    Create printing_pairs.cpp

---
+++ learning/beginner/arrays/printing_pairs.cpp
@@ -0,0 +1,32 @@
+//author: gautam narayan
+//assignment: print all possible pairs: eg. (1,2,3,) = (1,2) (1,3) (2,3) 
+
+#include <iostream>
+
+using namespace std;
+void printAllPairs(int arr[], int n);
+
+void printAllPairs(int arr[], int n) {
+
+    for (int i = 0; i < n; i++) {
+        int x = arr[i];
+        
+        for (int j = i + 1; j < n; j++) {
+            int y = arr[j];
+
+            cout << x << ", " << y << endl;
+        }
+        cout << endl;
+    }
+    
+}
+
+int main() {
+
+    int arr[] = { 10, 20, 30, 40, 50, 60 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printAllPairs(arr, n);
+
+    return 0;
+}
