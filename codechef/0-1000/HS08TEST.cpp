cecbd7735631ba41f1a4c570e72bdf4ddd499fd9
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2022-09-21 18:01:06 -0500

    Create HS08TEST.cpp

---
+++ codechef/0-1000/HS08TEST.cpp
@@ -0,0 +1,21 @@
+// author : gautam narayan
+
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+   cin.tie(nullptr) -> ios::sync_with_stdio(false);
+
+    double X{}, Y{};
+    cin >> X >> Y;
+
+    if (static_cast<int>(X) % 5 != 0) 
+        cout << Y << "\n";
+    else 
+        cout << fixed << setprecision(2) << Y - X - .50;
+
+    return 0;
+}
