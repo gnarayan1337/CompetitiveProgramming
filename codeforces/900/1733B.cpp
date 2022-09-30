1364d22e9fb8851791cd822859ed4b97bb5dea39
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2022-09-30 12:56:20 -0500

    Create 1733B.cpp

---
+++ codeforces/900/1733B.cpp
@@ -0,0 +1,47 @@
+// author : gautam narayan
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr) -> ios::sync_with_stdio(false);
+
+    int tc{}; cin >> tc;
+    while(tc--) {
+        int n{}, x{}, y{};
+        cin >> n >> x >> y;
+
+        // we can start by ruling impossible games
+        if (x == y) {
+            cout << -1 << "\n";
+            continue;
+        }
+        if (max(x, y) > 0 and min(x, y) > 0) {
+            cout << -1 << "\n";
+            continue;
+        }
+        else if (min(x, y) == 0 and ((n - 1) % max(x, y) != 0)) {
+            cout << -1 << "\n";
+            continue;
+        }
+
+        // if max(x, y) is > 0 and min(x, y) == 0 we can do a simple algorithm
+        if (max(x, y) == (n - 1)) {
+            for (int i = 0; i < max(x, y); i++) { cout << 1 << "\n"; }
+        }
+        else if ((n - 1) % max(x, y) == 0) {
+            int counter = 1;
+            for (int i = 0; i < ((n - 1) / max(x, y)); i++) {
+                for (int i = 0; i < max(x, y); i++) {
+                    cout << counter << " ";
+                }
+                counter += max(x, y) + 1;
+            }
+        }
+
+        cout << "\n";
+    }
+    return 0;
+}
