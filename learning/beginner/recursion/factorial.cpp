02993b1405fec0c614f84ba70232f1456f32a1ae
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2022-11-30 22:41:36 -0600

    Update factorial.cpp

--- learning/beginner/recursion/factorial.cpp
+++ learning/beginner/recursion/factorial.cpp
@@ -1,1 +1,28 @@
-asf
+// author : Gautam Narayan
+// date : 30 November 2022
+
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n);
+int factorial(int n) {
+
+    // base case
+    if (n == 0)
+        return 1;
+
+    // recursive case
+    int ans = n * factorial(n - 1);
+    return ans;
+}
+
+int main() {
+    cin.tie(nullptr) -> ios::sync_with_stdio(false);
+
+    int n{};
+    cin >> n;
+    cout << factorial(n) << "\n";
+
+    return 0;
+}
